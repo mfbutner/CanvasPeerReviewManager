6b848fb88bde2d3c0c2f8a11da48a04f3e692e7a
Gem Gasca <ggasca@ucdavis.edu>
2019-08-14 14:13:50 -0700

    making OO

--- Student Class.py
+++ Student Class.py
@@ -3,32 +3,43 @@ import canvasapi
 import csv
 import json
 
-class StudentReview:
-
-
-    def __init__(self, course, user_id):
+class Student:
+    def __init__(self, course, submission):
+        user_id = submission.user_id
         self.sis_login_id = course.get_user(user_id).login_id
         self.first, self.last = course.get_user(user_id).name.split()
         self.canvas_id = 0
         self.student_id = 0
-        self.total = 0
+        self.total = submission.score
         self.mean = 0
         self.median = 0
         self.mode = 0
         self.std_dev = 0
+        self.late = submission.late
 
+
+class StudentReview:
+    def __init__(self, course, review):
+        id = review.user_id
+        user = course.get_user(id)
+        self.first, self.last = user.name.split()
+        self.student_id =
+        self.canvas_id =
+        self.sis_login_id = user.login_id
+        self.total_score =
+        self.rubric = []
 
 
 class AssignmentPeerReviews:
 
-  def __init__(self, assignment):
-      self.name = assignment.name
-      self.assignment_id = assignment.id
-      self.reviewers = []
-      self.mean = 0
-      self.median = 0
-      self.mode = 0
-      self.std_dev = 0
+    def __init__(self, assignment):
+        self.name = assignment.name
+        self.assignment_id = assignment.id
+        self.reviewers = []
+        self.mean = 0
+        self.median = 0
+        self.mode = 0
+        self.std_dev = 0
 
 
 canvas = canvasapi.Canvas("https://canvas.ucdavis.edu", sys.argv[1]  )         # Make a new Canvas object
@@ -37,3 +48,4 @@ assignments = course.get_assignments()
 assignment = assignments[0]
 
 for submission in assignment.get_submissions():
+    student = Student(course, submission)
