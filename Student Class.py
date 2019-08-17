754baadea6ef65e1b345f0c2fb223b1d47550ff0
Gem Gasca <ggasca@ucdavis.edu>
2019-08-17 14:57:58 -0700

    working on getting peer review scores

--- Student Class.py
+++ Student Class.py
@@ -22,7 +22,7 @@ class Student:
         self.mode = self.mode_of_reviews(submission)
         self.std_dev = self.std_dev_of_reviews(submission)
 
-        self.this_students_reviews = self.get_reviews(self, course, assignment)
+        self.this_students_reviews = self.get_reviews(self, course, assignment, submission)
         self.rubric_stats = self.get_rubric_stats()
 
     @staticmethod
@@ -55,7 +55,7 @@ class Student:
         return std_dev
 
     @staticmethod
-    def get_reviews(self, course, assignment):  # reviews have attribute "to_json"
+    def get_reviews(self, course, assignment, submission):  # reviews have attribute "to_json"
         review_list = []
 
         for review in assignment.get_peer_reviews():
@@ -63,7 +63,7 @@ class Student:
             student_name = course.get_user(student_id).name
 
             if self.name == student_name:           # matches name of this student to name on reviewed assignment
-                reviewer = StudentReview(course, assignment, review)
+                reviewer = StudentReview(course, assignment, review, submission)
                 review_list.append(reviewer)
 
         return review_list
@@ -71,7 +71,7 @@ class Student:
 
 class StudentReview:
 
-    def __init__(self, course, assignment, review):
+    def __init__(self, course, assignment, review, submission):
         self.canvas_id = review.assessor_id
         self.sis_login_id = course.get_user(self.canvas_id).login_id
         self.student_id = 0
@@ -79,12 +79,14 @@ class StudentReview:
         self.reviewer_name = course.get_user(self.canvas_id).name
         self.first, self.second = self.reviewer_name.split()
 
-        self.rubric = self.get_reviewer_assessment(assignment, review)
+        self.rubric = self.get_reviewer_assessment(self, assignment, review, submission)
         self.total_score = 0
 
-    def get_reviewer_assessment(self, assignment, review):
+    @staticmethod
+    def get_reviewer_assessment(self, assignment, review, submission):
         assessment = {}
-
+        # print(submission.body)
+        # print(submission.get_submission_peer_reviews)
         return assessment
 
 
@@ -93,7 +95,6 @@ class AssignmentPeerReviews:
     def __init__(self, assignment):
         self.name = assignment.name
         self.assignment_id = assignment.id
-        self.reviewers = []
         self.mean = 0
         self.median = 0
         self.mode = 0
@@ -103,11 +104,13 @@ class AssignmentPeerReviews:
 def main():
     canvas = canvasapi.Canvas("https://canvas.ucdavis.edu", sys.argv[1])
     course = canvas.get_course(1599)
-    assignments = course.get_assignments()
-    assignment = assignments[0]
+    # assignments = course.get_assignments()
+    # assignment = assignments[0]
 
-    for submission in assignment.get_submissions():
-        student = Student(course, assignment, submission)
+    for assignment in course.get_assignments():
+        print("ASSIGNMENT:", assignment.name)
+        for submission in assignment.get_submissions():
+            student = Student(course, assignment, submission)
 
 
 main()
