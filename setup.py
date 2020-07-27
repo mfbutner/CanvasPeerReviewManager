be68be8e4569abb9688c206c2b557349a93ab68c
rezhv <al.havaei@icloud.com>
2020-07-27 16:29:38 -0700

    solved error handling and user invalid input

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Peer Reviewer",
+    version="1.0",
+    install_requires=["appdirs>=1.4.4",
+                        "py2app",
+                        "canvas-api>=0.1",
+                        "canvasapi>=0.15.0",
+                        "certifi>=2020.6",
+                        "chardet>=3.0.4",
+                        "distlib>=0.3.0",
+                        "filelock>=3.0.12",
+                        "methodtools>=0.1.2",
+                        "Pillow>=7.1.2",
+                        "Pillow-PIL>=0.1.dev0",
+                        "virtualenv>=20.0.21",
+                        "virtualenv-clone>=0.5.4",
+                        "wirerope>=0.3.1"""],
+    author="Alireza Havaei",
+    author_email="ahavaeishamsabadi@ucdavis.edu",
+    package_data={
+        # If any package contains *.jpg files, include them:
+        "": ["*.jpg"],
+    },
+
+    entry_points={
+        "gui_scripts": [
+            "peer-reviewer = peer_reviewer_program.rungui:run",
+            "peer-reviewer-terminal = peer_reviewer_program.runterminal.py"
+        ]
+    },
+    packages=find_packages(),
+)
